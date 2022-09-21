9aebd0d5654c1cef32e08117ac5608d44edfcb3e
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-21 14:32:29 +0800

    Refactor 'hello world' to RandomWords

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,9 +17,24 @@ class MyApp extends StatelessWidget {
           title: const Text("Welcome, Sincan Maulana"),
         ),
         body: const Center(
-          child: Text("Hello World"),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
